eea27fa4c7610cf60c6094c8d0ac92ad119db78d
Oliver Sun <olisun@mac.com>
2020-05-08 18:59:14 -0700

    installed ApolloServer axios and graphql plus nodemon. built server and schema in server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const { ApolloServer, gql } = require('apollo-server');
+const axios = require('axios');
+
+// Schema
+const typeDefs = gql`
+  type User {
+    id: ID 
+    login: String
+    avatar_url: String
+  }
+
+  type Query {
+    users: [User]
+  }
+`
+
+// Resolvers for fetching data from GQL
+const resolvers = {
+  Query: {
+    users: async () => {
+      try {
+        const users = await axios.get('https://api.github.com/users')
+        return users.data.map(({ id, login, avatar_url }) => ({
+          id,
+          login,
+          avatar_url
+        }))
+      } catch (error) {
+        throw error
+      }
+    }
+  }
+}
+
+// Creating the Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => console.log(`Server ready at ${url}`))
